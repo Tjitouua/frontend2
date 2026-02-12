c946a81d551b97d66bef04fdead563b10ceba065
Tjitouua Mapoha <tmapoha@GRN.GOV.NA>
2026-02-12 17:53:24 +0200

    Fixed the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,19 @@
 const Footer = () => {
       return (
          <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-3 lg:grid-cols-3 xl:grid-cols-4 px-10 py-6 bg-[var(--color-primary)] text-[var(--color-secondary-card)]">
-             <div className="py-3 bg-red-400 px-3">
+             <div className="py-3 px-3">
 
              </div>
 
-             <div className="py-3 bg-blue-400 px-3">
+             <div className="py-3 px-3">
 
              </div>
 
-             <div className="py-3 bg-red-400 px-3">
+             <div className="py-3 px-3">
 
              </div>
 
-             <div className="py-3 bg-blue-400 px-3
+             <div className="py-3 px-3
              ">
 
              </div>
